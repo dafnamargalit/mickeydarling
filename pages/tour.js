1541143d61c9584c3352a059e867c43cb7ad4165
Dafna <margalit.dafna77@gmail.com>
2023-11-26 22:28:22 -0800

    tour list

--- pages/tour.js
+++ pages/tour.js
@@ -3,9 +3,49 @@ import { Bus, BusFill } from 'assets';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import multipleStars from 'assets/multipleStars.png';
+import Link from 'next/link';
 
 export default function Tour() {
   var [busPathLength, setBusPathLength] = useState(null);
+
+  const shows = [
+    {
+        title: 'Madeline The Person w/ Dafna at The Moroccan Lounge',
+        date: 'Dec 7th 2023',
+        location: 'Los Angeles, CA',
+        link: 'https://www.ticketmaster.com/madeline-the-person-los-angeles-california-12-07-2023/event/09005F35F28047BC'
+    },    
+    {
+      title: 'Madeline The Person w/ Dafna at The Moroccan Lounge',
+      date: 'Dec 7th 2023',
+      location: 'Los Angeles, CA',
+      link: 'https://www.ticketmaster.com/madeline-the-person-los-angeles-california-12-07-2023/event/09005F35F28047BC'
+  },     
+  {
+    title: 'Madeline The Person w/ Dafna at The Moroccan Lounge',
+    date: 'Dec 7th 2023',
+    location: 'Los Angeles, CA',
+    link: 'https://www.ticketmaster.com/madeline-the-person-los-angeles-california-12-07-2023/event/09005F35F28047BC'
+},    
+{
+  title: 'Madeline The Person w/ Dafna at The Moroccan Lounge',
+  date: 'Dec 7th 2023',
+  location: 'Los Angeles, CA',
+  link: 'https://www.ticketmaster.com/madeline-the-person-los-angeles-california-12-07-2023/event/09005F35F28047BC'
+},    
+{
+  title: 'Madeline The Person w/ Dafna at The Moroccan Lounge',
+  date: 'Dec 7th 2023',
+  location: 'Los Angeles, CA',
+  link: 'https://www.ticketmaster.com/madeline-the-person-los-angeles-california-12-07-2023/event/09005F35F28047BC'
+},    
+{
+title: 'Madeline The Person w/ Dafna at The Moroccan Lounge',
+date: 'Dec 7th 2023',
+location: 'Los Angeles, CA',
+link: 'https://www.ticketmaster.com/madeline-the-person-los-angeles-california-12-07-2023/event/09005F35F28047BC'
+},     
+]
 
   useEffect(() => {
     var path = document.querySelector('.Bus');
@@ -13,14 +53,29 @@ export default function Tour() {
     if (path){
       setBusPathLength(path.getTotalLength());
     }
-    console.log(busPathLength);
   });
 
   return (
       <Container>
-            <Image src={multipleStars} layout="fill" objectFit="cover" alt="stars" priority />
+        <Image src={multipleStars} layout="fill" objectFit="cover" alt="stars" priority />
         <TourBus pathLength={busPathLength} />
         <BusColor />
+        <Header>
+        </Header>
+        <TourList>
+            {shows.map((show, index) => (
+                <ListItem key={index}>
+                    <ListSection>
+                        <Title>{show.date}</Title>
+                        <Title>{show.title}</Title>
+                    </ListSection>
+                    <p>{show.location}</p>
+                    <TicketButton href={show.link}>
+                        TICKETS
+                    </TicketButton>
+                </ListItem>
+            ))}
+        </TourList>
       </Container>
   );
 }
@@ -31,7 +86,19 @@ const Container = styled.div`
     justify-content: center;
     flex-direction: column;
     height: 100vh;
+    top: 0;
     overflow: hidden;
+`;
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+
+    @media (max-width: 768px) {
+        height: 20vh;
+    }
 `;
 
 const BusColor = styled(BusFill)`
@@ -39,6 +106,8 @@ width: 100vw;
 height: 100vh;
 z-index: 0;
 position: absolute;
+left: -2.5vw;
+transform: scaleX(1.1);
 `;
 
 const TourBus = styled(Bus)`
@@ -46,6 +115,8 @@ width: 100vw;
 height: 100vh;
 z-index: 1;
 position: absolute;
+left: -2.5vw;
+transform: scaleX(1.1);
 & path {
     fill: none;
     stroke: #000;
@@ -54,7 +125,7 @@ position: absolute;
     stroke-dashoffset: ${props => -props.pathLength}; 
     stroke-linecap: round;
     stroke-miter-limit: 10;
-    animation: dash 3s linear;   
+    animation: dash 2s linear;   
     animation-fill-mode: forwards;
     keypoints: 1, 0;
     keytimes: 0, 1;
@@ -67,3 +138,64 @@ position: absolute;
 }
 `;
 
+const TourList = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 40vw;
+  height: 62vh;
+  background: rgba(245, 158, 226, 0.90);
+  z-index: 2;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+
+  @media (max-width: 768px) {
+    width: 100vw;
+    height: 100vh;
+    background: rgba(245, 158, 226, 0.70);
+  }
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem;
+  border-bottom: 1px solid #000;
+`;
+
+const ListSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  width: 33%;
+`;
+
+const Title = styled.p`
+  margin: 0;
+`;
+
+const TicketButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  border: 1px solid #000;
+  background: #BB88FC;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    background: #BB88FD;
+    transform: scale(1.05);
+    transition: all 0.5s ease;
+  }
+`;
